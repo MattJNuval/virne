4613a84d65581e29d1eb7b9e87361d98d88ef518
mathew nuval <mathew.nuval.339@my.csun.edu>
2024-12-10 21:29:07 -0800

    First implementation of Tabu Search

--- virne/solver/meta_heuristic/__init__.py
+++ virne/solver/meta_heuristic/__init__.py
@@ -3,6 +3,7 @@ from .genetic_algorithm_solver import GeneticAlgorithmSolver
 from .particle_swarm_optimization_solver import ParticleSwarmOptimizationSolver
 from .meta_heuristic_solver import Individual, MetaHeuristicSolver, INFEASIBLE_FITNESS
 from .simulated_annealing_solver import SimulatedAnnealingSolver
+from .tabu_search_solver import TabuSearchSolver
 
 
 __all__ = [
@@ -12,5 +13,6 @@ __all__ = [
     'Individual',
     'MetaHeuristicSolver',
     'INFEASIBLE_FITNESS',
-    'SimulatedAnnealingSolver'
+    'SimulatedAnnealingSolver',
+    'TabuSearchSolver'
 ]
