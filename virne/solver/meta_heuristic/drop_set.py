a89ad01d2cc4fbd099b14bcdc22c685d08521978
mathew nuval <mathew.nuval.339@my.csun.edu>
2024-12-30 16:13:31 -0800

    Commented debug code

--- virne/solver/meta_heuristic/drop_set.py
+++ virne/solver/meta_heuristic/drop_set.py
@@ -10,17 +10,14 @@ class DropSet:
         if(self.drop_value >= self.global_drop_variable):
             self.drop_value = 0
         drop_set[self.drop_value] = item
-        self.drop_value = self.drop_value + 1        
-
-
-            
+        self.drop_value = self.drop_value + 1               
         
 
-if __name__ == "__main__":
-    drop_set = DropSet(6)
-    items = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six']
-    drop_set_results = {} 
-    for i in items:
-        drop_set.add_item(i, drop_set_results)
-    print(drop_set_results)
+# if __name__ == "__main__":
+#     drop_set = DropSet(6)
+#     items = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six']
+#     drop_set_results = {} 
+#     for i in items:
+#         drop_set.add_item(i, drop_set_results)
+#     print(drop_set_results)
     
