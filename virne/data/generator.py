e18e1cc987e29b3ba9859d741d78a11902e4b3ec
mathew nuval <mathew.nuval.339@my.csun.edu>
2025-02-22 16:57:06 -0800

    Implemented load function

--- virne/data/generator.py
+++ virne/data/generator.py
@@ -14,7 +14,7 @@ from virne.utils import get_p_net_dataset_dir_from_setting, get_v_nets_dataset_d
 class Generator:
 
     @staticmethod
-    def generate_dataset(config, p_net=True, v_nets=True, save=False):
+    def generate_dataset(config, p_net=True, v_nets=True, save=False, reuse_existing_p=False, reuse_existing_v=False):
         """
         Generate a dataset consisting of a physical network and a virtual network request simulator.
 
@@ -27,18 +27,18 @@ class Generator:
         Returns:
             Tuple: A tuple consisting of the generated physical network and virtual network request simulator.
         """
-        physical_network = Generator.generate_p_net_dataset_from_config(config, save=save) if p_net else None
-        v_net_simulator = Generator.generate_v_nets_dataset_from_config(config, save=save) if v_nets else None
+        physical_network = Generator.generate_p_net_dataset_from_config(config, save=save, reuse_existing=reuse_existing_p) if p_net else None
+        v_net_simulator = Generator.generate_v_nets_dataset_from_config(config, save=save, reuse_existing=reuse_existing_v) if v_nets else None
         return physical_network, v_net_simulator
 
     @staticmethod
-    def generate_p_net_dataset_from_config(config, save=False):
-        """
-        Generate a physical network dataset based on the given configuration.
+    def generate_p_net_dataset_from_config(config, save=False, reuse_existing=False):
+        """Generate a physical network dataset from config, reusing existing if specified.
 
         Args:
             config (dict or object): Configuration object containing the settings for the generator.
             save (bool): Whether or not to save the generated dataset.
+            reuse_existing (bool): Whether to reuse an existing physical network dataset if available.
 
         Returns:
             PhysicalNetwork: A PhysicalNetwork object representing the generated dataset.
@@ -49,7 +49,13 @@ class Generator:
         random.seed(config['seed'])
         np.random.seed(config['seed'])
         
-        p_net = PhysicalNetwork.from_setting(p_net_setting)
+        p_net_dataset_dir = get_p_net_dataset_dir_from_setting(p_net_setting) #moved to top
+        if reuse_existing and os.path.exists(p_net_dataset_dir):
+            p_net = PhysicalNetwork.load_dataset(p_net_dataset_dir)
+            if config.get('verbose', 1):
+                print(f'Loaded physical network dataset from {p_net_dataset_dir}')
+        else:
+           p_net = PhysicalNetwork.from_setting(p_net_setting)
 
         if save:
             p_net_dataset_dir = get_p_net_dataset_dir_from_setting(p_net_setting)
@@ -57,17 +63,15 @@ class Generator:
             if config.get('verbose', 1):
                 print(f'save p_net dataset in {p_net_dataset_dir}')
 
-        # new_p_net = PhysicalNetwork.load_dataset(p_net_dataset_dir)
-        return p_net
-
     @staticmethod
-    def generate_v_nets_dataset_from_config(config, save=False):
+    def generate_v_nets_dataset_from_config(config, save=False, reuse_existing=False):
         """
         Generate a virtual network request simulator dataset based on the given configuration.
 
         Args:
             config (dict or object): Configuration object containing the settings for the generator.
             save (bool): Whether or not to save the generated dataset.
+            reuse_existing (bool): Whether to reuse an existing virtual network dataset if available.
 
         Returns:
             VirtualNetworkRequestSimulator: A VirtualNetworkRequestSimulator object representing the generated dataset.
@@ -81,12 +85,17 @@ class Generator:
         v_net_simulator = VirtualNetworkRequestSimulator.from_setting(v_sim_setting)
         v_net_simulator.renew()
 
-        if save:
-            v_nets_dataset_dir = get_v_nets_dataset_dir_from_setting(v_sim_setting)
-            v_net_simulator.save_dataset(v_nets_dataset_dir)
+        v_nets_dataset_dir = get_v_nets_dataset_dir_from_setting(v_sim_setting) #moved to top
+        if reuse_existing and os.path.exists(v_nets_dataset_dir):
+            v_net_simulator = VirtualNetworkRequestSimulator.load_dataset(v_nets_dataset_dir)
             if config.get('verbose', 1):
-                print(f'save v_net dataset in {v_nets_dataset_dir}')
-
-        # new_v_net_simulator = VirtualNetworkRequestSimulator.load_dataset(v_nets_dataset_dir)
+                print(f'Loaded virtual network dataset from {v_nets_dataset_dir}')
+        else:
+            v_net_simulator = VirtualNetworkRequestSimulator.from_setting(v_sim_setting)
+            v_net_simulator.renew()  # generate new data.
+            if save:
+                v_net_simulator.save_dataset(v_nets_dataset_dir)
+                if config.get('verbose', 1):
+                    print(f'Saved virtual network dataset to {v_nets_dataset_dir}')
         return v_net_simulator
-        
+                
