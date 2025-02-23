54b2f8edb44d5fd3640a68c323580b6efceed798
mathew nuval <mathew.nuval.339@my.csun.edu>
2025-02-22 17:10:06 -0800

    Added ability to do arg inputs and fix reuse existing data condition on main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from virne.base import BasicScenario
 from virne import Config, REGISTRY, Generator, update_simulation_setting
 
+import sys
+
 
 def run(config):
     print(f"\n{'-' * 20}    Start     {'-' * 20}\n")
@@ -16,10 +18,24 @@ def run(config):
 
 
 if __name__ == '__main__':
-    config = Config(
-        solver_name='tsa',
-        # p_net_setting_path='customized_p_net_setting_file_path',
-        # v_sim_setting_path='customized_v_sim_setting_file_path',
-    )
-    Generator.generate_dataset(config, p_net=False, v_nets=False, save=False)
+    if(sys.argv[1] == 'ts'):
+        config = Config(
+            solver_name='ts',
+            # p_net_setting_path='customized_p_net_setting_file_path',
+            # v_sim_setting_path='customized_v_sim_setting_file_path',
+        )
+    elif(sys.argv[1] == 'ga'):
+        config = Config(
+            solver_name='ga',
+            # p_net_setting_path='customized_p_net_setting_file_path',
+            # v_sim_setting_path='customized_v_sim_setting_file_path',
+        )
+    elif(sys.argv[1] == 'tsa'):
+        config = Config(
+            solver_name='tsa',
+            # p_net_setting_path='customized_p_net_setting_file_path',
+            # v_sim_setting_path='customized_v_sim_setting_file_path',
+        )
+    
+    Generator.generate_dataset(config, p_net=False, v_nets=False, save=False, reuse_existing_v=False)
     run(config)
